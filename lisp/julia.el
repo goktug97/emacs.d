94076943149e2adf5fafc59244223b51a8a9088c
Göktuğ Karakaşlı <20567087+goktug97@users.noreply.github.com>
2020-05-21 22:44:35 +0300

    remove unused

--- lisp/julia.el
+++
@@ -1,4 +0,0 @@
-;; https://github.com/emacs-ess/ESS/wiki/Julia 
-(load "/home/goktug/libraries/ESS/lisp/ess-site")
-(setq inferior-julia-program-name "/home/goktug/libraries/julia/julia")
-(add-to-list 'ess-tracebug-search-path "/home/goktug/libraries/julia/base")
